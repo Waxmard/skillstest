5581d1216d56c83326757413761161ac2ed8f614
MaxwellWardWM <mtward01@email.wm.edu>
2021-09-03 09:30:09 -0400

    solution now highlights too many areas on the photo

--- Programming-Assignment-Data/solution.py
+++ Programming-Assignment-Data/solution.py
@@ -6,27 +6,16 @@ def main():
     tree = ET.parse('com.pandora.android.xml')
     root = tree.getroot()
 
-    # prints each child (only one) and its attributes
-    # for child in root:
-    #     print(child.tag, child.attrib)
-
-    print()
-
-    # prints all elements in the tree
-    # print([elem.tag for elem in root.iter()])
-
+    # saves all string bounds from each node
     allbounds = []
-
     for node in root.iter():
         attributes = node.attrib
         bounds = attributes.get('bounds')
         if (bounds is not None) and (bounds != "[0,0][1440,2368]"):
             allbounds.append(bounds)
 
-    print(allbounds)
-
-    intbounds = []
     # convert string bounds into list of 4 integer coordinates
+    intbounds = []
     for bound in allbounds:
         bound = bound.split(',')
         bound[0] = int(bound[0][1:])
@@ -35,29 +24,24 @@ def main():
         bound[1] = int(tmp[0])
         bound.insert(2, tmp[1])
         bound[2] = int(bound[2][1:])
-        print(bound)
         intbounds.append(bound)
-    print()
-    print(intbounds)
-    return intbounds
 
-    # String representation
-    # print(ET.tostring(root, encoding='utf8').decode('utf8'))
+    return intbounds
 
 def pillow(bounds):
     img = Image.open('com.pandora.android.png')
 
     draw = ImageDraw.Draw(img)
-    # draw.line((0, 0) + img.size, fill=128)
-    # draw.line((0, img.size[1], img.size[0], 0), fill=128)
-    # draw.line((200, 100, 300, 200), fill=(0, 0, 0), width=10)
-    draw.rectangle(
-        (80, 2119, 336, 2368),
+    
+    for bound in bounds:
+        draw.rectangle(
+        (bound[0], bound[1], bound[2], bound[3]),
         outline=(0, 0, 0),
-        width=10)
+        width=10
+        )
 
     img.show()
 
 if __name__ == "__main__":
-    allbounds = main()
-    # pillow(allbounds)
+    bounds = main()
+    pillow(bounds)
